53e49c19442d4ba3cda6068f2e7b331d2b11318e
Flavie Guiselin <flavie.sola.guiselin@gmail.com>
2024-03-12 11:46:40 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :cons do
+    resources :bookings, only: [:new, :create]
+  end
+  patch "accept", to: "bookings#accept"
+  patch "refuse", to: "bookings#refuse"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "profile", to: "pages#profile"
   # Defines the root path route ("/")
   # root "posts#index"
 end
