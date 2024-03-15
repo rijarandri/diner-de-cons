bc81980a0aca0e1c58cfad0982adc2bd80e89935
Flavie Guiselin <flavie.sola.guiselin@gmail.com>
2024-03-15 11:41:07 +0100

    add bookings on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
 
   has_many :bookings, dependent: :destroy
-=======
->>>>>>> master
   has_many :cons
 end
